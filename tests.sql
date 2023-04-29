52d2d581e8f2205d9048d568543214e2c2f9b74d
NunoBartolomeu <a47233@alunos.isel.pt>
2023-04-29 16:32:18 +0100

    0

---
+++ tests.sql
@@ -0,0 +1,21 @@
+-- Exercise D
+
+-- Exercise E
+
+-- Exercise F
+
+-- Exercise G
+
+-- Exercise H
+
+-- Exercise I
+
+-- Exercise J
+
+-- Exercise K
+
+-- Exercise L
+
+-- Exercise M
+
+-- Exercise N
