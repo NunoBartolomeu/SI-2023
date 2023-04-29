ac51e9a1c24bad84092f42f64c64a06c8bb323a2
NunoBartolomeu <a47233@alunos.isel.pt>
2023-04-29 15:41:09 +0100

    create done

--- createTables.sql
+++ createTables.sql
@@ -11,23 +11,26 @@ CREATE TABLE IF NOT EXISTS Jogadores (
     email VARCHAR(255) NOT NULL,
     estado ESTADO_JOGADOR NOT NULL DEFAULT 'ativo',
     regiao VARCHAR(255) NOT NULL,
+    
 	CONSTRAINT fk_regiao FOREIGN KEY (regiao) REFERENCES Regioes(nome)
 );
 
 CREATE TABLE IF NOT EXISTS Estatisticas_Jogador (
-    id SERIAL PRIMARY KEY,
     id_jogador INT NOT NULL,
     num_jogos INT NOT NULL,
     num_partidas INT NOT NULL,
     total_pontos INT NOT NULL,
 
+    CONSTRAINT pk_jogador PRIMARY KEY (id_jogador),
+    
     CONSTRAINT fk_jogador FOREIGN KEY (id_jogador) REFERENCES Jogadores(id)
 );
 
 CREATE TABLE IF NOT EXISTS Amigos (
-    id SERIAL PRIMARY KEY,
     id_jogador1 INT NOT NULL,
     id_jogador2 INT NOT NULL,
+
+    CONSTRAINT pk_amigos PRIMARY KEY (id_jogador1, id_jogador2),
 
     CONSTRAINT fk_jogador1 FOREIGN KEY (id_jogador1) REFERENCES Jogadores(id),
     CONSTRAINT fk_jogador2 FOREIGN KEY (id_jogador2) REFERENCES Jogadores(id)
@@ -39,107 +42,119 @@ CREATE TABLE IF NOT EXISTS Conversas (
 );
 
 CREATE TABLE IF NOT EXISTS Participantes_Conversa (
-    id SERIAL PRIMARY KEY,
     id_conversa INT NOT NULL, 
     id_jogador INT NOT NULL,
+
+    CONSTRAINT pk_jogador_conversa PRIMARY KEY (id_jogador, id_conversa),
 
     CONSTRAINT fk_conversa FOREIGN KEY (id_conversa) REFERENCES Conversas(id),
     CONSTRAINT fk_jogador FOREIGN KEY (id_jogador) REFERENCES Jogadores(id)
 );
 
 CREATE TABLE IF NOT EXISTS Mensagens (
-    id SERIAL PRIMARY KEY,
+    numero SERIAL,
     id_conversa INT NOT NULL,
     id_jogador INT NOT NULL,
     texto VARCHAR(255) NOT NULL,
     data TIMESTAMP NOT NULL,
+
+    CONSTRAINT pk_mensagens PRIMARY KEY (numero, id_conversa),
 
     CONSTRAINT fk_conversa FOREIGN KEY (id_conversa) REFERENCES Conversas(id),
     CONSTRAINT fk_jogador FOREIGN KEY (id_jogador) REFERENCES Jogadores(id)
 );
 
 CREATE TABLE IF NOT EXISTS Jogos (
-    id SERIAL PRIMARY KEY,
+    id VARCHAR(10) PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
     url VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Estatisticas_Jogo (
-    id SERIAL PRIMARY KEY,
-    id_jogo INT NOT NULL,
+    id_jogo VARCHAR(10) NOT NULL,
     num_jogadores INT NOT NULL,
     num_partidas INT NOT NULL,
     total_pontos INT NOT NULL,
+
+    CONSTRAINT pk_jogo PRIMARY KEY (id_jogo),
 
     CONSTRAINT fk_jogo FOREIGN KEY (id_jogo) REFERENCES Jogos(id)
 );
 
 CREATE TABLE IF NOT EXISTS Compras (
-    id SERIAL PRIMARY KEY,
     id_jogador INT NOT NULL,
-    id_jogo INT NOT NULL,
+    id_jogo VARCHAR(10) NOT NULL,
     data_de_compra TIMESTAMP NOT NULL,
     preco FLOAT NOT NULL,
+
+    CONSTRAINT pk_jogo_jogador PRIMARY KEY (id_jogador, id_jogo),
 
     CONSTRAINT fk_jogador FOREIGN KEY (id_jogador) REFERENCES Jogadores(id),
     CONSTRAINT fk_jogo FOREIGN KEY (id_jogo) REFERENCES Jogos(id)
 );
 
 CREATE TABLE IF NOT EXISTS Crachas (
-    id SERIAL PRIMARY KEY,
-    id_jogo INT NOT NULL,
     nome VARCHAR(255) NOT NULL,
+    id_jogo VARCHAR(10) NOT NULL,
     url VARCHAR(255) NOT NULL,
     limite_pontos INT NOT NULL,
+
+    CONSTRAINT pk_crachas primary key (id_jogo, nome),
 
     CONSTRAINT fk_jogo FOREIGN KEY (id_jogo) REFERENCES Jogos(id)
 );
 
 CREATE TABLE IF NOT EXISTS Crachas_Obtidos (
-    id SERIAL PRIMARY KEY,
     id_jogador INT NOT NULL,
-    id_cracha INT NOT NULL,
+    nome_cracha VARCHAR(255) NOT NULL,
+	id_jogo VARCHAR(10) NOT NULL,
+	
+    CONSTRAINT pk_jogador_cracha_jogo PRIMARY KEY (id_jogador, id_jogo, nome_cracha),
 
     CONSTRAINT fk_jogador FOREIGN KEY (id_jogador) REFERENCES Jogadores(id),
-    CONSTRAINT fk_cracha FOREIGN KEY (id_cracha) REFERENCES Crachas(id)
+    CONSTRAINT fk_cracha FOREIGN KEY (nome_cracha, id_jogo) REFERENCES Crachas(nome, id_jogo)
 );
 
 CREATE TABLE IF NOT EXISTS Partida (
-    id SERIAL PRIMARY KEY,
-    id_jogo INT NOT NULL,
+    id SERIAL,
+    id_jogo VARCHAR(10) NOT NULL,
     data_inicio TIMESTAMP NOT NULL,
     data_fim TIMESTAMP NOT NULL,
     regiao VARCHAR(255) NOT NULL,
 
     CONSTRAINT fk_jogo FOREIGN KEY (id_jogo) REFERENCES Jogos(id),
-    CONSTRAINT fk_regiao FOREIGN KEY (regiao) REFERENCES Regioes(nome)
+    CONSTRAINT fk_regiao FOREIGN KEY (regiao) REFERENCES Regioes(nome),
+    CONSTRAINT pk_jogo_partida PRIMARY KEY (id, id_jogo)
 );
 
 CREATE TABLE IF NOT EXISTS Partida_Normal (
-    id SERIAL PRIMARY KEY,
     id_partida INT NOT NULL,
+    id_jogo VARCHAR(10) NOT NULL,
     dificuldade INT NOT NULL,
 
-    CONSTRAINT fk_partida FOREIGN KEY (id_partida) REFERENCES Partida(id)
+    CONSTRAINT fk_partida FOREIGN KEY (id_partida, id_jogo) REFERENCES Partida(id, id_jogo),
+    CONSTRAINT pk_partida PRIMARY KEY (id_partida, id_jogo)
 );
 
 DROP TYPE IF EXISTS ESTADO_PARTIDA;
 CREATE TYPE ESTADO_PARTIDA AS ENUM ('em curso', 'terminada');
 
 CREATE TABLE IF NOT EXISTS Partida_Multi_Jogador (
-    id SERIAL PRIMARY KEY,
     id_partida INT NOT NULL,
+    id_jogo VARCHAR(10) NOT NULL,
     estado ESTADO_PARTIDA NOT NULL DEFAULT 'em curso',
 
-    CONSTRAINT fk_partida FOREIGN KEY (id_partida) REFERENCES Partida(id)
+    CONSTRAINT fk_partida FOREIGN KEY (id_partida, id_jogo) REFERENCES Partida(id, id_jogo),
+    CONSTRAINT pk_partidaMult PRIMARY KEY (id_partida, id_jogo)
 );
 
 CREATE TABLE IF NOT EXISTS Pontuacao (
-    id SERIAL PRIMARY KEY,
     id_partida INT NOT NULL,
     id_jogador INT NOT NULL,
+    id_jogo VARCHAR(10) NOT NULL,
     pontos INT NOT NULL,
 
-    CONSTRAINT fk_partida FOREIGN KEY (id_partida) REFERENCES Partida(id),
-    CONSTRAINT fk_jogador FOREIGN KEY (id_jogador) REFERENCES Jogadores(id)
+    CONSTRAINT fk_partida FOREIGN KEY (id_partida, id_jogo) REFERENCES Partida(id, id_jogo),
+    CONSTRAINT fk_jogador FOREIGN KEY (id_jogador) REFERENCES Jogadores(id),
+    CONSTRAINT pk_pontuaçao PRIMARY KEY (id_partida, id_jogador, id_jogo)
 );
--- insertToTables.sql
+++ insertToTables.sql
@@ -1,11 +1,94 @@
-INSERT INTO table1 (id, name, age) VALUES (1, 'John', 20);
-INSERT INTO table1 (id, name, age) VALUES (2, 'Mary', 21);
-INSERT INTO table1 (id, name, age) VALUES (3, 'Peter', 22);
-INSERT INTO table1 (id, name, age) VALUES (4, 'Susan', 23);
-INSERT INTO table1 (id, name, age) VALUES (5, 'Jennifer', 24);
-INSERT INTO table1 (id, name, age) VALUES (6, 'Michael', 25);
-INSERT INTO table1 (id, name, age) VALUES (7, 'William', 26);
-INSERT INTO table1 (id, name, age) VALUES (8, 'Richard', 27);
-INSERT INTO table1 (id, name, age) VALUES (9, 'Karen', 28);
-INSERT INTO table1 (id, name, age) VALUES (10, 'Lisa', 29);
-INSERT INTO table1 (id, name, age) VALUES (11, 'Betty', 30);
+INSERT INTO Regioes (nome) VALUES
+('Portugal'),
+('Espanha');
+
+INSERT INTO Jogadores (username, email, estado, regiao) VALUES
+('Rui', 'rui@themail.com', 'ativo', 'Portugal'),
+('João', 'joao@themail.com', 'inativo', 'Portugal'),
+('Carlos', 'carlos@themail.com', 'ativo', 'Espanha'),
+('Julio', 'julio@themail.com', 'banido', 'Espanha'),
+
+INSERT INTO Estatisticas_Jogador (id_jogador, num_jogos, num_partidas, total_pontos) VALUES
+(1, 2, 2, 200),
+(2, 1, 4, 400),
+(3, 1, 1, 100),
+(4, 2, 2, 200);
+
+INSERT INTO Amigos (id_jogador1, id_jogador2) VALUES
+(1, 2), (1,3),
+(2, 1), (2, 3),
+(3, 1), (3, 2), (3, 3),
+(4, 3);
+
+INSERT INTO Conversas (nome) VALUES
+('Portugueses'),
+('Españoles');
+
+INSERT INTO Participantes_Conversa (id_conversa, id_jogador) VALUES
+(1, 1), (1, 2),
+(2, 3), (2, 4);
+
+INSERT INTO Mensagens (id_conversa, id_jogador, texto, data) VALUES
+(1, 1, 'Boas', '2017-01-01 12:30:00'),
+(1, 2, 'Boas', '2017-01-01 12:30:12'),
+(1, 1, 'Bora?', '2017-01-01 12:31:16'),
+(1, 1, 'Yah', '2017-01-01 12:31:59');
+
+INSERT INTO Jogos (nome, url) VALUES
+('League of Legends', 'www.lol.com'),
+('Minecraft', 'www.mc.com'),
+
+INSERT INTO Estatisticas_Jogo (id_jogo, num_jogadores, num_partidas, total_pontos) VALUES
+(1, 4, 7, 700),
+(2, 2, 2, 200);
+
+INSERT INTO Compras (id_jogador, id_jogo, data_de_compra, preco) VALUES
+(1, 1, '2017-01-01', 0),
+(1, 2, '2017-01-01', 20),
+(2, 1, '2017-01-02', 0),
+(3, 1, '2017-01-03', 0),
+(4, 1, '2017-01-04', 0),
+(4, 2, '2017-01-04', 20),
+
+INSERT INTO Crachas (id_jogo, nome, url, limite_pontos) VALUES
+(1, 'begin', 'www.lol.com/img/begin', 10),
+(1, 'intermediate', 'www.lol.com/img/intermediate', 100),
+(1, 'advanced', 'www.lol.com/img/advanced', 1000),
+(2, 'begin', 'www.mc.com/img/begin', 10),
+(2, 'intermediate', 'www.mc.com/img/intermediate', 100),
+(2, 'advanced', 'www.mc.com/img/advanced', 1000);
+
+INSERT INTO Crachas_Obtidos (id_jogador, id_cracha) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(2, 1),
+(2, 2),
+(3, 1),
+(4, 1),
+(4, 2),
+(4, 3);
+
+INSERT INTO Partida (id_jogo, data_inicio, data_fim, regiao) VALUES
+(1, '2017-01-03 10:10', '2017-01-03 10:20', 'Portugal'),
+(1, '2017-01-04 12:12', '2017-01-04 12:22', 'Portugal'),
+(1, '2017-01-05 14:14', '2017-01-05 14:24', 'Portugal'),
+(2, '2017-01-06 16:16', '2017-01-06 16:26', 'Espanha'),
+
+INSERT INTO Partida_Normal (id_partida, id_jogo, dificuldade) VALUES
+(1, 2, 5);
+
+INSERT INTO Partida_Multi_Jogador (id_partida, id_jogo, estado) VALUES
+(1, 1, "terminada"),
+(1, 2, "em curso"),
+(1, 3, "em curso");
+
+INSERT INTO Pontuacao (id_partida, id_jogo, id_jogador, pontos) VALUES
+(1, 1, 1, 50),
+(1, 1, 2, 50),
+(2, 1, 3, 100),
+(2, 1, 4, 100),
+(3, 1, 1, 50),
+(3, 1, 2, 50),
+(4, 1, 3, 100),
+(4, 1, 4, 100);
