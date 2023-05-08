1fc1ea4e38857adb64ac64c5ce4a545e5b867197
j-vie <j-viegas@sapo.pt>
2023-05-08 20:13:41 +0100

    A paginação estva off e adicionei os extras ao createTables

--- createTables.sql
+++ createTables.sql
@@ -162,3 +162,43 @@ CREATE TABLE IF NOT EXISTS Pontuacoes (
     CONSTRAINT fk_jogador FOREIGN KEY (id_jogador) REFERENCES Jogadores(id),
     CONSTRAINT pk_pontuaçao PRIMARY KEY (id_partida, id_jogador, id_jogo)
 );
+
+
+-- Update automático das estatisticas
+CREATE OR REPLACE FUNCTION atualizar_estatisticas()
+    RETURNS TRIGGER AS $$
+    BEGIN
+        -- Atualize as estatísticas do jogador correspondente
+        UPDATE Estatisticas_Jogador
+        SET
+            num_jogos = num_jogos + 1,
+            num_partidas = num_partidas + 1,
+            total_pontos = total_pontos + NEW.pontos
+        WHERE id_jogador = NEW.id_jogador;
+
+        RETURN NEW;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_atualizar_estatisticas
+AFTER INSERT OR UPDATE ON Pontuacoes
+FOR EACH ROW
+EXECUTE FUNCTION atualizar_estatisticas();
+
+
+
+-- Criação das estatisticas com trigger
+CREATE OR REPLACE FUNCTION criacao_estatisticas_jogador()
+    RETURNS TRIGGER AS $$
+    BEGIN
+        -- Atualize as estatísticas do jogador correspondente
+        INSERT INTO Estatisticas_Jogador
+		VALUES (NEW.id, 0, 0, 0);
+        RETURN NEW;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_criacao_estatisticas_jogador
+AFTER INSERT ON jogadores
+FOR EACH ROW
+EXECUTE FUNCTION criacao_estatisticas_jogador();
